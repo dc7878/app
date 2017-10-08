4b98a9b55b70eebd7a9b91ff7d92e03db20ebf22
dc7878 <1026961500@qq.com>
2017-10-08 20:32:49 +0800

    添加common依赖

--- memorytraining/src/main/java/com/cc/dc/memorytraining/ui/home/activity/MainActivity.java
+++ memorytraining/src/main/java/com/cc/dc/memorytraining/ui/home/activity/MainActivity.java
@@ -1,15 +1,25 @@
 package com.cc.dc.memorytraining.ui.home.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.cc.dc.common.ui.BaseActivity;
 import com.cc.dc.memorytraining.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayoutId() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    public void initView(Bundle savedInstanceState) {
+
+    }
+
 }
