c6049884b0b1d6fc9f2c839f8f13064f8020f69e
dc7878 <1026961500@qq.com>
2017-10-08 19:36:54 +0800

    新建一个memory-training的demo

---
+++ memorytraining/src/main/java/com/cc/dc/memorytraining/AppApplication.java
@@ -0,0 +1,15 @@
+package com.cc.dc.memorytraining;
+
+import android.app.Application;
+
+/**
+ * Created by dc on 17/10/8.
+ */
+
+public class AppApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ memorytraining/src/main/java/com/cc/dc/memorytraining/activity/A.java
@@ -0,0 +1,8 @@
+package com.cc.dc.memorytraining.activity;
+
+/**
+ * Created by dc on 17/10/8.
+ */
+
+public class A {
+}
