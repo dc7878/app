f266d1aa55301f75b558920757ec37c9e91916f0
dc7878 <1026961500@qq.com>
2017-10-12 17:40:52 +0800

    添加圆形图片的库

--- demo/src/main/java/com/cc/dc/ui/follow/fragment/FollowFragment.java
+++ demo/src/main/java/com/cc/dc/ui/follow/fragment/FollowFragment.java
@@ -1,12 +1,19 @@
 package com.cc.dc.ui.follow.fragment;
 
+import com.bumptech.glide.Glide;
 import com.cc.dc.common.ui.BaseFragment;
 import com.cc.dc.dc.R;
+
+import butterknife.Bind;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by dc on 2017/9/19.
  */
 public class FollowFragment extends BaseFragment {
+
+    @Bind(R.id.profile_image)
+    CircleImageView circleImageView;
 
     @Override
     public int getLayoutId() {
@@ -15,16 +22,14 @@ public class FollowFragment extends BaseFragment {
 
     @Override
     public void initView() {
-
     }
 
     @Override
     public void initPresenter() {
-
     }
 
     @Override
     public void lazyLoadData() {
-
+        Glide.with(this).load("http://www.learn2sleep.com/icon.jpg").into(circleImageView);
     }
 }
