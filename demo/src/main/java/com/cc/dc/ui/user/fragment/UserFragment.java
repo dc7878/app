40ae59b154fdc64b317b3fe5fedf9bb296c8999c
dc7878 <1026961500@qq.com>
2017-11-01 15:07:39 +0800

    一些自定义的和动画效果

--- demo/src/main/java/com/cc/dc/ui/user/fragment/UserFragment.java
+++ demo/src/main/java/com/cc/dc/ui/user/fragment/UserFragment.java
@@ -1,12 +1,39 @@
 package com.cc.dc.ui.user.fragment;
 
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.cc.dc.common.animation.Rotate3dAnimation;
 import com.cc.dc.common.ui.BaseFragment;
 import com.cc.dc.R;
+
+import butterknife.Bind;
+import butterknife.OnClick;
 
 /**
  * Created by dc on 2017/9/19.
  */
 public class UserFragment extends BaseFragment {
+
+    @Bind(R.id.rl_content)
+    RelativeLayout content;
+    @Bind(R.id.iv_logo)
+    ImageView imageView;
+    @Bind(R.id.tv_desc)
+    TextView textView;
+
+    private int centerX;
+    private int centerY;
+    private int depthZ = 400;
+    private int duration = 300;
+    private Rotate3dAnimation openAnimation;
+    private Rotate3dAnimation closeAnimation;
+    private boolean isOpen = false;
 
     @Override
     public int getLayoutId() {
@@ -15,16 +42,110 @@ public class UserFragment extends BaseFragment {
 
     @Override
     public void initView() {
-
     }
 
     @Override
     public void initPresenter() {
-
     }
 
     @Override
     public void lazyLoadData() {
+    }
 
+    @OnClick(R.id.btn_show_dialog)
+    public void showDialog(View view) {
+        //以旋转对象的中心点为旋转中心点，这里主要不要再onCreate方法中获取，因为视图初始绘制时，获取的宽高为0
+        centerX = content.getWidth()/2;
+        centerY = content.getHeight()/2;
+        if (openAnimation == null) {
+            initOpenAnim();
+            initCloseAnim();
+        }
+
+        //用作判断当前点击事件发生时动画是否正在执行
+        if (openAnimation.hasStarted() && !openAnimation.hasEnded()) {
+            return;
+        }
+        if (closeAnimation.hasStarted() && !closeAnimation.hasEnded()) {
+            return;
+        }
+
+        //判断动画执行
+        if (isOpen) {
+            content.startAnimation(closeAnimation);
+        }else {
+            content.startAnimation(openAnimation);
+        }
+        isOpen = !isOpen;
+    }
+
+    /**
+     * 卡牌文本介绍打开效果：注意旋转角度
+     */
+    private void initOpenAnim() {
+        //从0到90度，顺时针旋转视图，此时reverse参数为true，达到90度时动画结束时视图变得不可见，
+        openAnimation = new Rotate3dAnimation(0, 90, centerX, centerY, depthZ, true);
+        openAnimation.setDuration(duration);
+        openAnimation.setFillAfter(true);
+        openAnimation.setInterpolator(new AccelerateInterpolator());
+        openAnimation.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.setVisibility(View.GONE);
+                textView.setVisibility(View.VISIBLE);
+
+                //从270到360度，顺时针旋转视图，此时reverse参数为false，达到360度动画结束时视图变得可见
+                Rotate3dAnimation rotateAnimation = new Rotate3dAnimation(270, 360, centerX, centerY, depthZ, false);
+                rotateAnimation.setDuration(duration);
+                rotateAnimation.setFillAfter(true);
+                rotateAnimation.setInterpolator(new DecelerateInterpolator());
+                content.startAnimation(rotateAnimation);
+            }
+        });
+    }
+
+    /**
+     * 卡牌文本介绍关闭效果：旋转角度与打开时逆行即可
+     */
+    private void initCloseAnim() {
+        closeAnimation = new Rotate3dAnimation(360, 270, centerX, centerY, depthZ, true);
+        closeAnimation.setDuration(duration);
+        closeAnimation.setFillAfter(true);
+        closeAnimation.setInterpolator(new AccelerateInterpolator());
+        closeAnimation.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.setVisibility(View.VISIBLE);
+                textView.setVisibility(View.GONE);
+
+                Rotate3dAnimation rotateAnimation = new Rotate3dAnimation(90, 0, centerX, centerY, depthZ, false);
+                rotateAnimation.setDuration(duration);
+                rotateAnimation.setFillAfter(true);
+                rotateAnimation.setInterpolator(new DecelerateInterpolator());
+                content.startAnimation(rotateAnimation);
+            }
+        });
     }
 }
